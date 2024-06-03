b7d33bf7c02fc25f97629b53a38525761bec4562
Benjamin Stürmer <webmaster@stuermer-benjamin.de>
2024-06-03 05:43:15 +0200

    adjust test

--- backend/src/main/kotlin/pro/stuermer/dailyexpenses/backend/Application.kt
+++ backend/src/main/kotlin/pro/stuermer/dailyexpenses/backend/Application.kt
@@ -31,10 +31,12 @@ import io.ktor.server.plugins.cors.routing.CORS
 import io.ktor.server.plugins.defaultheaders.DefaultHeaders
 import io.ktor.server.plugins.ratelimit.RateLimit
 import io.ktor.server.plugins.ratelimit.RateLimitName
+import io.ktor.server.request.httpMethod
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
+import org.slf4j.event.Level
 import pro.stuermer.dailyexpenses.shared.VersionInfo
 import java.io.File
 import kotlin.reflect.typeOf
@@ -93,7 +95,15 @@ fun Application.mainModule() {
         json()
     }
 
-    install(CallLogging)
+    install(CallLogging) {
+        level = Level.DEBUG
+        format { call ->
+            val status = call.response.status()
+            val httpMethod = call.request.httpMethod.value
+            val userAgent = call.request.headers["User-Agent"]
+            "Status: $status, HTTP method: $httpMethod, User agent: $userAgent"
+        }
+    }
 
     // Automatic '304 Not Modified' Responses
     install(ConditionalHeaders)
